c64357817d392122eda6f1b84c0444cd562c09a0
BPitts8019 <bradleyj.pitts@gmail.com>
2019-11-08 19:02:13 -0700

    Update colors complete

--- client/src/components/ColorList.js
+++ client/src/components/ColorList.js
@@ -1,89 +1,125 @@
 import React, { useState } from "react";
-import axios from "axios";
+import {axiosWithAuth as axios} from "../utils/api";
 
 const initialColor = {
-  color: "",
-  code: { hex: "" }
+   color: "",
+   code: { hex: "" }
 };
 
 const ColorList = ({ colors, updateColors }) => {
-  console.log(colors);
-  const [editing, setEditing] = useState(false);
-  const [colorToEdit, setColorToEdit] = useState(initialColor);
+   console.log(colors);
+   const [editing, setEditing] = useState(false);
+   const [colorToEdit, setColorToEdit] = useState(initialColor);
 
-  const editColor = color => {
-    setEditing(true);
-    setColorToEdit(color);
-  };
+   const editColor = color => {
+      setEditing(true);
+      setColorToEdit(color);
+   };
 
-  const saveEdit = e => {
-    e.preventDefault();
-    // Make a put request to save your updated color
-    // think about where will you get the id from...
-    // where is is saved right now?
-  };
+   const saveEdit = e => {
+      e.preventDefault();
+      // Make a put request to save your updated color
+      // think about where will you get the id from...
+      // where is is saved right now?
 
-  const deleteColor = color => {
-    // make a delete request to delete this color
-  };
+      // const colorIdx = colors.findIndex(color => color.id === colorToEdit.id);
+      
+      // const colorsEnd = colors.slice(colorIdx+1 );
+      // const newColors = [
+      //    colorsStart,
+      //    colorToEdit,
+      //    colorsEnd
+      // ];
 
-  return (
-    <div className="colors-wrap">
-      <p>colors</p>
-      <ul>
-        {colors.map(color => (
-          <li key={color.color} onClick={() => editColor(color)}>
-            <span>
-              <span className="delete" onClick={e => {
-                    e.stopPropagation();
-                    deleteColor(color)
-                  }
-                }>
-                  x
-              </span>{" "}
-              {color.color}
-            </span>
-            <div
-              className="color-box"
-              style={{ backgroundColor: color.code.hex }}
-            />
-          </li>
-        ))}
-      </ul>
-      {editing && (
-        <form onSubmit={saveEdit}>
-          <legend>edit color</legend>
-          <label>
-            color name:
+      //close editing form
+      setEditing(false);
+      let colorIdx;
+      let newColors = colors.filter((color, idx) => {
+         const result = color.id !== colorToEdit.id;
+
+         if (color.id === colorToEdit.id) {
+            colorIdx = idx;
+         }
+
+         return result;
+      });
+
+      console.log(newColors.slice(colorIdx));
+      newColors = [].concat(
+         newColors.slice(0,colorIdx),
+         [colorToEdit],
+         newColors.slice(colorIdx)
+      );
+
+      axios()
+         .put(`/api/colors/${colorToEdit.id}`, colorToEdit)
+         .then(response => {
+            updateColors(newColors);
+         })
+         .catch(err => {
+            console.error(err.response);
+         })
+   };
+
+   const deleteColor = color => {
+      // make a delete request to delete this color
+   };
+
+   return (
+      <div className="colors-wrap">
+         <p>colors</p>
+         <ul>
+            {colors.map(color => (
+               <li key={color.color} onClick={() => editColor(color)}>
+                  <span>
+                     <span className="delete" onClick={e => {
+                           e.stopPropagation();
+                           deleteColor(color)
+                        }
+                     }>x</span>
+                     {" "}{color.color}
+                  </span>
+                  <div
+                     className="color-box"
+                     style={{ backgroundColor: color.code.hex }}
+                  />
+               </li>
+            ))}
+         </ul>
+         {editing && (
+            <form onSubmit={saveEdit}>
+               <legend>edit color</legend>
+               <label>
+                  color name:
             <input
-              onChange={e =>
-                setColorToEdit({ ...colorToEdit, color: e.target.value })
-              }
-              value={colorToEdit.color}
-            />
-          </label>
-          <label>
-            hex code:
+                     onChange={e =>
+                        setColorToEdit({ ...colorToEdit, color: e.target.value })
+                     }
+                     value={colorToEdit.color}
+                  />
+               </label>
+               <label>
+                  hex code:
             <input
-              onChange={e =>
-                setColorToEdit({
-                  ...colorToEdit,
-                  code: { hex: e.target.value }
-                })
-              }
-              value={colorToEdit.code.hex}
-            />
-          </label>
-          <div className="button-row">
-            <button type="submit">save</button>
-            <button onClick={() => setEditing(false)}>cancel</button>
-          </div>
-        </form>
-      )}
-      <div className="spacer" />
-      {/* stretch - build another form here to add a color */}
-    </div>
-  );
+                     onChange={e =>
+                        setColorToEdit({
+                           ...colorToEdit,
+                           code: { hex: e.target.value }
+                        })
+                     }
+                     value={colorToEdit.code.hex}
+                  />
+               </label>
+               <div className="button-row">
+                  <button type="submit">save</button>
+                  <button onClick={() => setEditing(false)}>cancel</button>
+               </div>
+            </form>
+         )}
+         <div className="spacer" />
+         {/* stretch - build another form here to add a color */}
+      </div>
+   );
 };
 
 export default ColorList;
